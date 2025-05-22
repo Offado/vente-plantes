caa0a3e1f32a37c9d8743bcc7eae0277fd8a2352
offolome <offolomebillchrisjonathanadon@gmail.com>
2025-05-22 13:32:58 +0000

    initial

--- src/Components/PlantItem.js
+++ src/Components/PlantItem.js
@@ -2,8 +2,8 @@ import React from "react";
 import "../Styles/PlantItem.css";
 import CareScale from "./CareScale";
 
-// Fonction pour afficher le composant enfant CareScale et les propriétés de plantList 
-function PlantItem({ id, name, water, light, cover }) {
+// Fonction pour afficher le composant enfant CareScale et les propriétés de plantList
+function PlantItem({ id, cover, name, water, light }) {
   return (
     <li key={id} className="lmj-plant-item">
       <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
